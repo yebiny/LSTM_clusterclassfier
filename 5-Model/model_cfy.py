a2f6330d0a664e5b807a34876668ab304d4e5b69
graduated student from University of Seoul <yyoun@gate2.sscc.uos.ac.kr>
2019-06-19 13:47:54 +0900

    Saperate model develop code

--- 5-Model/model_cfy.py
+++ 5-Model/model_cfy.py
@@ -30,17 +30,6 @@ from rnn_cfy import build_model
 '''   python3 model.py [title] [number of epochs]  '''
 '''                                                '''
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''
-def build_model(x_shape):
-    
-    model = Sequential()
-    model.add(Bidirectional(LSTM(64, return_sequences=True), input_shape=(x_shape[1],x_shape[2])))
-    model.add(Bidirectional(LSTM(128)))
-    model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(32, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
-    return model
 
 def getyinfo(model, xset):
     
---
+++ 5-Model/version_model/rnn_cfy.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input, Bidirectional, Dropout
+from tensorflow.keras.utils import Sequence, plot_model
+from tensorflow.keras.models import load_model
+if tf.test.is_gpu_available(cuda_only=True):
+    from tensorflow.keras.layers import CuDNNLSTM as LSTM
+else:
+    from tensorflow.keras.layers import LSTM
+
+
+def build_model(x_shape):
+
+    model = Sequential()
+    model.add(Bidirectional(LSTM(64, return_sequences=True), input_shape=(x_shape[1],x_shape[2])))
+    model.add(Bidirectional(LSTM(128)))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+    return model
+
